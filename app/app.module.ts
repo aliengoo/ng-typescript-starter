876dbb8af512d0f92fc2d4a5973fb4fa850557d9
Glenn Bunter <glenn.android@gmail.com>
2015-06-01 23:57:41 +0100

    Reference paths

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,9 @@
 /// <reference path="../typings/angularjs/angular.d.ts"/>
 /// <reference path="./home/home.module.ts"/>
 
+///<reference path="app.controller.ts"/>
+///<reference path="app.config.ts"/>
+
 module app {
 
   angular.module('app', [
--- app/common/directives/top-bar.directive.ts
+++ app/common/directives/top-bar.directive.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../typings/angularjs/angular.d.ts"/>
+
 module common.directives {
 	export function topBar(): angular.IDirective {
 		return {
